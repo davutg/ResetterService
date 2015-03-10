c3befda6d9f52cc28d01468fbd1d65a75980cc51
davutg <davutgurbuz@gmail.com>
2015-03-10 10:28:02 +0200

    A complete project without an application config

---
+++ ResetterService/Helpers.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResetterService
+{
+    public partial class Helpers
+    {
+        public static string ApplicationExecutableName
+        {
+            get {
+                return System.AppDomain.CurrentDomain.FriendlyName;
+            }
+        }
+
+        public static string SendMail(string mailtoAddress, string mailSubject, string mailContent, string mailCCAddress = "", string mailBccAddress = "")
+        {
+            try
+            {
+                string smptpServer = ConfigurationManager.AppSettings["SmtpServer"];
+                string smtpServerAuthenticateUser = ConfigurationManager.AppSettings["SmtpServerAuthenticateUser"];
+                string smtpServerAuthenticatePassword = Helpers.Decrypt(ConfigurationManager.AppSettings["SmtpServerAuthenticatePassword"]);
+                int smtpServerPort = Int32.Parse(ConfigurationManager.AppSettings["SmtpServerPort"]);
+                string fromEmailAddress = ConfigurationManager.AppSettings["fromEmailAddress"];
+
+                MailMessage mail = new MailMessage();
+                mail.IsBodyHtml = true;
+                mail.Body = mailContent;
+
+                System.Net.Mime.ContentType contentType = new System.Net.Mime.ContentType("text/html");
+                contentType.Name = mailSubject + ".html";
+
+                //Geçici çözümdür
+                if (mailContent.Contains("charset=utf-8"))
+                {
+                    mailContent = mailContent.Replace("charset=utf-8", "charset=iso-8859-9");
+                }
+
+                byte[] mailContentBytes = Encoding.Default.GetBytes(mailContent);
+                Stream stream = new MemoryStream(mailContentBytes);
+                Attachment mailAttach = new Attachment(stream, contentType);
+                mail.Attachments.Add(mailAttach);
+                //}
+
+                mail.Subject = mailSubject;
+                mail.Sender = new MailAddress(string.Format(fromEmailAddress, "{0} Uygulaması", ApplicationExecutableName));
+                mail.From = mail.Sender;
+
+
+                foreach (var toAddress in SplitMailList(mailtoAddress))
+                {
+                    mail.To.Add(new MailAddress(toAddress));
+                }
+
+                foreach (var ccAddress in SplitMailList(mailCCAddress))
+                {
+                    mail.CC.Add(new MailAddress(ccAddress));
+                }
+
+
+                foreach (var bccAddress in SplitMailList(mailBccAddress))
+                {
+                    mail.Bcc.Add(new MailAddress(bccAddress));
+                }
+
+                mail.Priority = MailPriority.High;
+                mail.BodyEncoding = Encoding.UTF8;
+
+                System.Net.Mail.SmtpClient smtpClient = new SmtpClient(smptpServer, smtpServerPort);
+                smtpClient.EnableSsl = false;
+                smtpClient.Credentials = new NetworkCredential(smtpServerAuthenticateUser, smtpServerAuthenticatePassword);
+
+                smtpClient.Send(mail);
+                return "ok";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+
+        public static List<string> SplitMailList(string concatedAddresses)
+        {
+            if (!string.IsNullOrEmpty(concatedAddresses))
+            {
+                return concatedAddresses.Split(';').ToList();
+            }
+            else return new List<string>();
+        }
+
+    }
+}
---
+++ ResetterService/RestarterService.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResetterService
+{
+    public partial class RestarterService : ServiceBase
+    {
+        private static System.Timers.Timer ResetterJob;
+        public static string[] ServiceNames = null ;
+        private static Lazy<ConfigFileConfigurationProvider> configuration = new Lazy<ConfigFileConfigurationProvider>(() =>
+            {
+                ConfigFileConfigurationProvider configProvider = new ConfigFileConfigurationProvider();
+                return configProvider;
+            }
+            , true);
+
+        public RestarterService()
+        {
+            InitializeComponent();
+            Init();            
+        }
+
+        private void Init()
+        {
+            ReadConfig();           
+            try
+            {
+                ResetterJob = new System.Timers.Timer(TimeSpan.FromHours(1).TotalMilliseconds); //Satte bir kontrol et.
+                ResetterJob.Elapsed += new System.Timers.ElapsedEventHandler(ResetterJob_Elapsed);
+                ResetterJob.Start();
+            }
+            catch (Exception excep)
+            {                        
+                this.EventLog.WriteEntry("Init error:" + excep.Message, EventLogEntryType.Error);
+            }
+            finally{
+                logInfo("ApplicationStarted");
+            }                                
+        }
+
+        private void ReadConfig()
+        {
+            ServiceNames = configuration.Value.GetServiceNames();
+            foreach (var service in ServiceNames)
+            {
+                Console.WriteLine(service);
+            }
+        }
+
+        private void Reset(string serviceName)
+        {
+            // Check whether the Alerter service is started.
+
+            ServiceController sc = new ServiceController();
+            
+            sc.ServiceName = serviceName;
+            string infoMessageCurrentStatus=string.Format("The {0} service status is currently set to {1}",serviceName, sc.Status.ToString());
+            logInfo(infoMessageCurrentStatus);
+
+            if (sc.Status == ServiceControllerStatus.Running)
+            {
+                logInfo(string.Format("Stopping the {0} service...", serviceName));
+                sc.Stop();
+                sc.WaitForStatus(ServiceControllerStatus.Stopped);
+
+                logInfo(string.Format("The {0} service status is now set to {1}.", serviceName, sc.Status.ToString()));
+            }
+
+            if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                // Start the service if the current status is stopped.
+
+                logInfo(string.Format("Starting the {0} service...",serviceName));
+                try
+                {
+                    // Start the service, and wait until its status is "Running".
+                    sc.Start();
+                    sc.WaitForStatus(ServiceControllerStatus.Running);
+
+                    // Display the current service status.
+                    logInfo(string.Format("The {0} service status is now set to {1}.",serviceName, sc.Status.ToString()));
+
+                }
+                catch (InvalidOperationException)
+                {
+                    string errorMessage=string.Format("Could not start the {0} service.",serviceName);
+                    logError(errorMessage);
+                }
+            }
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        void logInfo(string infoMessage)
+        {
+            Console.WriteLine(infoMessage);
+            this.EventLog.WriteEntry(infoMessage, EventLogEntryType.Information);
+            logCommon("[INFO]->" + infoMessage);
+        }
+
+        void logError(string errorMessage)
+        { 
+            Console.WriteLine(errorMessage);
+            this.EventLog.WriteEntry(errorMessage, EventLogEntryType.Error);
+            string mailToAddress=ConfigFileConfigurationProvider.configuration.Value.AppSettings.Settings["mailtoAddress"].Value;
+            string mailCCAddress=ConfigFileConfigurationProvider.configuration.Value.AppSettings.Settings["mailCCAddress"].Value;
+            Helpers.SendMail(mailToAddress, "ResetterAppliciton", errorMessage, mailCCAddress);
+            logCommon("[ERROR]->"+errorMessage);
+        }
+
+        void logCommon(string text)
+        {
+            logToFile(text);
+        }
+
+        void logToFile(string text)
+        {
+            string path = string.Format("{0}_{1}.txt", Helpers.ApplicationExecutableName ,DateTime.Now.ToString("yyyy-MM-dd"));
+            using (StreamWriter sw = (File.Exists(path)) ? File.AppendText(path) : File.CreateText(path))
+            {                
+                    sw.WriteLine(text);
+                    sw.Flush();
+            } 
+        }
+
+        void ResetterJob_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                var hours=ConfigFileConfigurationProvider.configuration.Value.AppSettings.Settings["WorkingHours"].Value.Split(',');
+                if (!hours.Contains(DateTime.Now.Hour.ToString()))
+                    return;
+
+                ResetterJob.Stop();
+                foreach (var serviceName in ServiceNames)
+                {
+                    Reset(serviceName);
+                }
+            }
+            catch (Exception ex)
+            {
+                string errorMessage="ResetterJob servis Hatası :" + ex == null ? "exception is null" : "ResetterJob servis Hatası  :" + ex.Message ?? "exception.Message null";
+                logError(errorMessage);
+            }
+            finally
+            {
+                ResetterJob.Start();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RestarterService service = new RestarterService();
+
+            if (Environment.UserInteractive)
+            {
+                service.OnStart(args);
+                Console.WriteLine("Press any key to stop program");
+                Console.Read();
+                service.OnStop();
+            }
+            else
+            {
+                ServiceBase.Run(service);
+            }
+
+        }
+
+    }
+
+    public interface IServiceNameProvider
+    {
+        string[] GetServiceNames();
+    }
+
+    public partial class ConfigFileConfigurationProvider : IServiceNameProvider
+    {
+        public static Lazy<Configuration> configuration = new Lazy<Configuration>(() =>
+            {
+                Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                return configuration;
+            },
+            true);
+
+        public string[] GetServiceNames()
+        {
+            string serviceNamesToReset=configuration.Value.AppSettings.Settings["ServiceNamesToRestart"].Value;    
+            var serviceNamesArray = serviceNamesToReset.Split(',');
+            return serviceNamesArray;
+        }
+    }
+}
